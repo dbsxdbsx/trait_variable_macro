f3e30f2e0e9eda63328bfe4f17c3bdf7300ee3d0
dbsxdbsx <djw0214@gmail.com>
2024-01-21 16:02:29 +0800

    refine

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,8 @@ pub fn trait_var(attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     let expanded = quote! {
-        trait_enhance! {
-            #[trait_enhance(#attribute_name)]
+        trait_variable! {
+            #[trait_variable(#attribute_name)]
             #input
         }
     };
