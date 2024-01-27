ab917493f3815ecb2fd4efa4d6c74c3f746f3999
dbsxdbsx <djw0214@gmail.com>
2024-01-27 09:45:55 +0800

    refine

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
     parse::Parse, parse::ParseStream, parse_macro_input, AttributeArgs, DeriveInput, ItemStruct,
     ItemTrait, Lit, Meta, MetaNameValue, NestedMeta,
@@ -9,20 +9,24 @@ use syn::{
 #[proc_macro_attribute]
 pub fn trait_var(args: TokenStream, input: TokenStream) -> TokenStream {
     // 解析属性输入
-    let args = parse_macro_input!(args as Meta);
-    let trait_name = match args {
-        Meta::Path(path) => path.get_ident().unwrap().to_string(),
+    let args = parse_macro_input!(args as syn::AttributeArgs);
+    let trait_name = match args.first().unwrap() {
+        syn::NestedMeta::Meta(syn::Meta::Path(path)) => path.get_ident().unwrap().to_string(),
         _ => panic!("Expected a trait name"),
     };
 
     // 解析输入结构体
     let input_struct = parse_macro_input!(input as ItemStruct);
+    let struct_name = &input_struct.ident;
+    let struct_fields = input_struct.fields.iter().map(ToTokens::to_token_stream);
 
     // 生成新的代码
     let expanded = quote! {
         trait_variable! {
             (#trait_name)
-            struct #input_struct
+            struct #struct_name {
+                #(#struct_fields),*
+            }
         }
     };
 
@@ -30,7 +34,7 @@ pub fn trait_var(args: TokenStream, input: TokenStream) -> TokenStream {
     expanded.into()
 }
 
-// for tag trait
+// TODO: for tag trait, not ok yet
 // #[proc_macro_attribute]
 // pub fn trait_var(_attr: TokenStream, item: TokenStream) -> TokenStream {
 //     let input = parse_macro_input!(item as ItemTrait);
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,169 +1,84 @@
-use trait_variable_macros::trait_var;
+#[cfg(test)]
+mod test {
+    use trait_variable::trait_variable;
+    use trait_variable_macros::trait_var;
 
-pub trait TraitEnhanceType<'a> {
-    type View;
-    type ViewMut;
-}
+    //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓trait definition↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    trait_variable! {
+        // #[trait_var]
+        // the lint is also activated inside the macro, using rust_anaylzer for example
+        trait MyTrait {  // feel free to add `pub` when needed
+            // 1.put the variable fields definition at the top of the target trait before any function
+            let x: i32; // TODO: can't be without variable at present
+            let y: bool;
+            let z : f32;
 
-pub trait TraitEnhance: for<'a> TraitEnhanceType<'a> {
-    fn get_fields(&self) -> <Self as TraitEnhanceType<'_>>::View;
-    fn get_fields_mut(&mut self) -> <Self as TraitEnhanceType<'_>>::ViewMut;
-}
+            // 2.the order of the function definition doesn't matter
+            // fn print_x(&self); // ok
+            fn print_x(&self) {
+                // println!("(original)x: `{}`", self.x);// TODO: make self.<> valid
+                println!("(original)x: `{}`", self._x());
+            }
+            fn print_y(&self);
+            fn print_z(&self);
+            fn ret_bool(&self) -> bool;
 
-#[macro_export]
-macro_rules! trait_variable {
-    // Parsing trait (has more fields):
-    (@enhance_trait
-        trait_def = $trait_def:tt,
-        content = {
-            $(#[$field_attr:meta])*
-            let $field_name:ident: $field_type:ty;
-            $($trait_content:tt)*
-        },
-        fields = { $($prev_fields:tt)* },
-        dollar = {$dollar:tt},
-    ) => {
-        $crate::trait_variable! {
-            @enhance_trait  // NOTE: this is a recursive call
-            trait_def = $trait_def,
-            content = { $($trait_content)* },
-            fields = {
-                $($prev_fields)*
-                $(#[$field_attr])*
-                let $field_name: $field_type;
-            },
-            dollar = {$dollar},
+            // fn change_and_print_z(&mut self, new_num: f32) {
+            //     *self.get_fields_mut().z = new_num;
+            //     println!("z: `{}`", self.get_fields().z);
+            // }
         }
-    };
-    // Parsing trait (finished, trait content doesn't start with a field so rest is the real trait):
-    (@enhance_trait
-        trait_def = {
-            $(#[$attr:meta])*
-            $vis:vis trait $trait_name:ident
-        },
-        content = { $($trait_content:tt)* },
-        fields = { $(
-            $(#[$field_attr:meta])*
-            let $field_name:ident: $field_type:ty;
-        )* },
-        dollar = {$dollar:tt},
-    ) => {
-        paste::paste! {
-            $(#[$attr])*
-            $vis trait $trait_name:
-                $crate::TraitEnhance
-                + for<'a> $crate::TraitEnhanceType<'a,
-                    View = [< $trait_name _View >]<'a>,
-                    ViewMut = [< $trait_name _ViewMut >]<'a>
-                >
-            {
-                $($trait_content)*
-            }
-            #[doc(hidden)]
-            #[allow(non_camel_case_types, dead_code)]
-            pub struct [< $trait_name _View >]<'a> {
-                $($vis $field_name: &'a $field_type,)*
-            }
-            impl<'a> [< $trait_name _View >]<'a> {
-                $vis fn new($($field_name: &'a $field_type),*) -> Self {
-                    Self { $($field_name,)* }
-                }
-            }
-            #[doc(hidden)]
-            #[allow(non_camel_case_types, dead_code)]
-            pub struct [< $trait_name _ViewMut >]<'a> {
-                $($vis $field_name: &'a mut $field_type,)*
-            }
-            impl<'a> [< $trait_name _ViewMut >]<'a> {
-                $vis fn new($($field_name: &'a mut $field_type),*) -> Self {
-                    Self { $($field_name,)* }
-                }
-            }
-            //  the struct macro part
-            #[doc(hidden)]
-            #[macro_export] // <-- Only if the trait's visibility is `pub`
-            macro_rules! __temp_macro_name {
-                (
-                    $dollar (#[$dollar struct_attr:meta])*
-                    $dollar vis:vis struct $dollar struct_name:ident {
-                        $dollar ( $dollar struct_content:tt )*
-                    }
-                ) => {
-                    $dollar (#[$dollar struct_attr])*
-                    $dollar vis struct $dollar struct_name {
-                        $dollar ( $dollar  struct_content)*
-                        // From outer macro:
-                        $(
-                            $(#[$field_attr])*
-                            $field_name: $field_type,
-                        )*
-                    }
-                    impl<'a> $crate::TraitEnhanceType<'a> for $struct_name {
-                        type View = <dyn $trait_name as $crate::TraitEnhanceType<'a>>::View;
-                        type ViewMut = <dyn $trait_name as $crate::TraitEnhanceType<'a>>::ViewMut;
-                    }
-                    impl $crate::TraitEnhance for $struct_name {
-                        fn get_fields(&self) -> <Self as $crate::TraitEnhanceType<'_>>::View {
-                            <Self as $crate::TraitEnhanceType>::View::new($(
-                                &self.$field_name,
-                            )*)
-                        }
-                        fn get_fields_mut(&mut self) -> <Self as $crate::TraitEnhanceType<'_>>::ViewMut {
-                            <Self as $crate::TraitEnhanceType>::ViewMut::new($(
-                                &mut self.$field_name,
-                            )*)
-                        }
-                    }
-                };
-            }
-            // Expose this macro under the same name as the trait:
-            $vis use __temp_macro_name as $trait_name; // without this, arm `2` can't be triggered
-        }
-    };
-    // 2. Entry point for parsing a struct, to generated macro next to the trait:
-    (
-        #[trait_var($trait:path)] // this line is just used as a tag
-        // ($trait:path) // this line is just used as a tag
-        $(#[$attr:meta])*
-        $vis:vis struct $struct_name:ident {
-            $(
-                $(#[$field_attr:meta])*
-                $field_vis:vis $field_name:ident : $field_type:ty
-            ),* $(,)?
-        }
-    ) => {
-        // the `$trait！` is doing the `__temp_macro_name` job
-        $trait! {
-            $(#[$attr])*
-            $vis struct $struct_name {
-                $(
-                    $(#[$field_attr:meta])*
-                    $field_vis $field_name : $field_type,
-                )*
-            }
-        }
-    };
-    // 1. Entry point for parsing a trait:
-    (
-        $(#[$attr:meta])*
-        $vis:vis trait $trait_name:ident {
-            $($trait_content:tt)*
-        }
-    ) => {
-        $crate::trait_variable!{
-            @enhance_trait  // NOTE: this is a recursive call
-            trait_def = {
-                $(#[$attr])*
-                $vis trait $trait_name
-            },
-            content = { $($trait_content)* },
-            fields = {},
-            dollar = {$},
-        }
-    };
-}
+    }
+    //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑trait definition↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
-#[trait_var(MyTrait)]
-struct MyStruct {
-    a: i32,
+    //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓struct definition↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    #[trait_var(MyTrait)]
+    struct MyStruct {
+        a: i32,
+    }
+    // trait_variable! {
+    //     (MyTrait) // put this at the top of the struct
+    //     struct MyStruct { // feel free to add `pub` when needed
+    //     // feel free to add any fields as usual or leave it empty
+    //      a: i32,
+    //     }
+    // }
+    //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑struct definition↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+    //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓impl block↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    impl MyStruct {
+        fn print_a(&self) {
+            println!("a: `{}`", self.a);
+        }
+    }
+    impl MyTrait for MyStruct {
+        // fn print_x(&self) {
+        //     println!("x: `{}`", self.x);
+        // }
+        fn print_y(&self) {
+            println!("y: `{}`", self.y);
+        }
+        fn print_z(&self) {
+            println!("z: `{}`", self.z);
+        }
+
+        fn ret_bool(&self) -> bool {
+            true
+        }
+    }
+    //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑impl block↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+    #[test]
+    fn test() {
+        let mut s = MyStruct {
+            a: 2,
+            x: 3,
+            y: true,
+            z: 1.,
+        };
+        s.print_a();
+        s.print_x();
+        s.print_y();
+        // s.change_and_print_z(3.14);
+    }
 }
