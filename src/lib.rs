12bef3abe90de5941e25374efb923ba2d296c41c
dbsxdbsx <djw0214@gmail.com>
2024-01-29 23:10:40 +0800

    test import hidden trait path

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ItemStruct};
+use syn::{parse_macro_input, Ident, ItemStruct, Path, PathSegment};
 
 #[proc_macro_attribute]
 pub fn trait_var(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -11,7 +11,15 @@ pub fn trait_var(args: TokenStream, input: TokenStream) -> TokenStream {
         syn::NestedMeta::Meta(syn::Meta::Path(path)) => (path, path.get_ident().unwrap()),
         _ => panic!("Expected a trait name"),
     };
-
+    // create hidden_trait_path
+    let mut hidden_trait_path = trait_path.clone();
+    if let Some(last_segment) = hidden_trait_path.segments.last_mut() {
+        let ident = Ident::new(
+            &format!("_{}", last_segment.ident),
+            last_segment.ident.span(),
+        );
+        last_segment.ident = ident;
+    }
     // parse input, only accept `struct`
     let input_struct = parse_macro_input!(input as ItemStruct);
     let visible = &input_struct.vis;
@@ -20,7 +28,7 @@ pub fn trait_var(args: TokenStream, input: TokenStream) -> TokenStream {
 
     // expand code
     let expanded = quote! {
-        use #trait_path;
+        use #hidden_trait_path;
         trait_variable! {
             (#trait_name)
             #visible struct #struct_name {
